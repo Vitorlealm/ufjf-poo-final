29ab12786041e32f5d2edd55f6259a3a34be54fb
Vitor Leal <vitorlealm.21@gmail.com>
2023-11-10 19:21:58 -0300

    Primeiro commit

---
+++ ufjf-poo/src/main/java/org/example/Produto.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public class Produto {
+    private Long idProduto;
+    private String nome;
+    private BigDecimal valor;
+    //polimorfismo para calcular o preco;
+}
