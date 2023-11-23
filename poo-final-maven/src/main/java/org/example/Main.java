c9cdb5421d1e2da317dcf155e1679b5ed829831f
Vitor Leal <vitorlealm.21@gmail.com>
2023-11-22 21:42:02 -0300

    Adiciona as dependencias Swing e GSON

--- poo-final-maven/src/main/java/org/example/Main.java
+++ poo-final-maven/src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Teste!");
         Usuario usuario = new Usuario();
-        usuario.setCpf();
+        JFrame s = new JFrame("Chat 555555555555555555");
+        s.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        s.setSize(400, 400);
+        s.setVisible(true);
+        usuario.teste();
     }
 }
--- poo-final-maven/src/main/java/org/example/Usuario.java
+++ poo-final-maven/src/main/java/org/example/Usuario.java
@@ -2,6 +2,8 @@ package org.example;
 
 import lombok.Data;
 
+import javax.swing.*;
+
 @Data
 public class Usuario {
 
@@ -11,4 +13,11 @@ public class Usuario {
     private String cpf;
     private String endereco;
     private boolean admin;
+
+    public void teste(){
+        JFrame frame = new JFrame("Chat Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
 }
