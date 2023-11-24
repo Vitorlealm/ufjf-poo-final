da6eaf3e38c367400b6b6a0fc0c371bc853b5ae0
Davi <daviaireslage@gmail.com>
2023-11-24 14:27:03 -0300

    Inserindo Construtor, getters e setters na classe Pedido

--- poo-final-maven/src/main/java/org/example/Pedido.java
+++ poo-final-maven/src/main/java/org/example/Pedido.java
@@ -13,4 +13,52 @@ public class Pedido {
     private String enderecoEntrega;
     private BigDecimal valorTotal;
     private Date dataCriacao;
+
+    public Pedido(Long clienteId, List<Produto> listaProdutos, String enderecoEntrega, BigDecimal valorTotal, Date dataCriacao) {
+        this.clienteId = clienteId;
+        this.listaProdutos = listaProdutos;
+        this.enderecoEntrega = enderecoEntrega;
+        this.valorTotal = valorTotal;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Long getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Long clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public List<Produto> getListaProdutos() {
+        return listaProdutos;
+    }
+
+    public void setListaProdutos(List<Produto> listaProdutos) {
+        this.listaProdutos = listaProdutos;
+    }
+
+    public String getEnderecoEntrega() {
+        return enderecoEntrega;
+    }
+
+    public void setEnderecoEntrega(String enderecoEntrega) {
+        this.enderecoEntrega = enderecoEntrega;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
 }
