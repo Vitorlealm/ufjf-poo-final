13db5b2678b067b664f9fc249d4634866c287da3
Bruno <brunonascimentorodrigues2@hotmail.com>
2023-12-01 20:51:44 -0300

    Adicionando Método ValidarCPF

--- poo-final-maven/src/main/java/org/example/Usuario.java
+++ poo-final-maven/src/main/java/org/example/Usuario.java
@@ -67,4 +67,45 @@ public class Usuario {
         frame.add(button);
         frame.setVisible(true);
     }
+
+
+ public static boolean validarCPF(String cpf) {
+        // Removendo caracteres não numéricos
+        cpf = cpf.replaceAll("[^0-9]", "");
+        
+        // Verificando se o CPF tem 11 dígitos
+        if (cpf.length() != 11) {
+            return false;
+        }
+        
+        // Verificando CPFs com todos os dígitos iguais
+        if (cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+        
+        // Calculando o primeiro dígito verificador
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += (10 - i) * Integer.parseInt(String.valueOf(cpf.charAt(i)));
+        }
+        int digito1 = 11 - (soma % 11);
+        if (digito1 > 9) {
+            digito1 = 0;
+        }
+        
+        // Calculando o segundo dígito verificador
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += (11 - i) * Integer.parseInt(String.valueOf(cpf.charAt(i)));
+        }
+        int digito2 = 11 - (soma % 11);
+        if (digito2 > 9) {
+            digito2 = 0;
+        }
+        
+        // Verificando se os dígitos calculados são iguais aos dígitos informados
+        return (digito1 == Integer.parseInt(String.valueOf(cpf.charAt(9))) &&
+                digito2 == Integer.parseInt(String.valueOf(cpf.charAt(10))));
+    }
+ 
 }
